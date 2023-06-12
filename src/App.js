bcc21c019bd586c34cc81474a88effc4a56ae7ce
yangxiaotao <yangxiaotao@yuuwei.com>
2023-06-12 18:20:35 +0800

    axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 class App extends Component{
+  getStudentData = () => {
+    axios.get().then(res => {
+
+    }).catch(err => {
+
+    })
+  }
   render() {
     return (
-      <div></div>
+      <div>
+        <button onClick={this.getStudentData}>获取数据</button>
+      </div>
     )
   }
 }
